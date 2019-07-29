5d47e37ddc8510c3460647a51282384c21b1ffb4
erik.johansson <erikjohansson890@gmail.com>
2019-07-29 16:49:18 +0200

    need to render charssss

--- axelarigatotest/src/components/Films.js
+++ axelarigatotest/src/components/Films.js
@@ -7,7 +7,9 @@ class Films extends Component {
       super(props)
         this.state = {
           movies: [],
-          sortedList: []
+          showInfoMovie: false,
+          movieInfo: [],
+          characters: []
   
       }//State
   }//Contructo
@@ -20,7 +22,50 @@ class Films extends Component {
         return a.release_date.slice(0,4) - b.release_date.slice(0,4)
     })
     this.setState({ movies: list });
+    console.log(this.state.movies)
     })
+    
+    
+   
+  }
+
+  showMovie = (movie) => {
+    this.setState({showInfoMovie: true})
+    
+    let self = this.state.movies
+    for (var i=0; i < self.length; i++) {
+     if(self[i].title === movie) {
+    //  console.log(self[i].characters)
+
+      axios.all(self[i].characters.map(chars => axios.get(chars)))
+      .then(axios.spread(function(...res) {
+       // console.log(res)
+        let charList = res
+        let nameList = []
+        for(let i=0; i < charList.length; i++) {
+         //console.log(charList[i].data.name)
+          nameList.push(charList[i].data.name)
+          
+         
+
+
+          console.log(nameList)
+        }
+        //this.setState({characters: nameList})
+      }))
+     
+      
+      
+      //this.setState({movieInfo:self[i].characters})
+      
+     }//if
+    }//forLoop
+  }//showMovie
+
+  hideMovie = () => {
+    
+    console.log("iojppz")
+    this.setState({showInfoMovie: false})
   }
   
 render() {
@@ -28,13 +73,23 @@ render() {
     <div className="Films">
       <h3>All Star Wars Movies:</h3>
         <ul>
-          { this.state.movies.length == 0
+          { this.state.movies.length === 0
             ? <h2>Loading movies...</h2>
             :
             this.state.movies.map(movie => <li key={movie.title}>
-          <button>Star Wars, Episode:{movie.episode_id} Date: {movie.release_date}</button>
+          <button onClick={() => this.showMovie(movie.title)}>Star Wars, Episode:{movie.episode_id} Date: {movie.release_date}</button>
+          
         </li>)}
       </ul>
+      {
+        this.state.showInfoMovie ?
+        <div className="movieInfo">{this.state.movieInfo}
+          <button onClick={() => this.hideMovie()}>Go Back</button>
+        
+        </div>
+        :null
+      }
+      
     </div>
     );
     }
