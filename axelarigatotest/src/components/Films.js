9ace0d4949e8a864ad3b4ddae0690b759a6005a4
erik.johansson <erikjohansson890@gmail.com>
2019-07-30 10:43:03 +0200

    done

--- axelarigatotest/src/components/Films.js
+++ axelarigatotest/src/components/Films.js
@@ -11,94 +11,85 @@ class Films extends Component {
           movieInfo: [],
           characters: [],
           movieTitle: '',
-          movieTitle1: ''
-  
-      }//State
-     this.showMovie = this.showMovie.bind(this) 
+        }//State
+    this.showMovie = this.showMovie.bind(this) 
   }//Contructo
   
   componentDidMount() {
     axios.get('https://swapi.co/api/films')
-    .then(res => {
-      let list = res.data.results
+      .then(result => {
+        let movieListSortByDate = result.data.results
      
-      list.sort(function(a,b) {
-        return a.release_date.slice(0,4) - b.release_date.slice(0,4)
-    })
-    this.setState({ movies: list });
-    console.log(this.state.movies)
+        movieListSortByDate.sort(function(a,b) {
+          return a.release_date.slice(0,4) - b.release_date.slice(0,4)
+      })
+    this.setState({ movies: movieListSortByDate });
   })
-}
+}//ComponentDidMount
 
-
-
-  showMovie = (movie) => {
-    
+showMovie = (movie) => {
   this.setState({showInfoMovie: true})
     
-    let self = this.state.movies
-    for (var i=0; i < self.length; i++) {
-     if(self[i].title === movie) {
-    console.log(self[i].title)
-    this.setState({movieTitle: self[i].title })
-
-    axios.all(self[i].characters.map(chars => axios.get(chars)))
-      .then(axios.spread((...res) => {
-        //console.log(res)
-        let nameList = res
-        let charList = []
-       
-        for(let i=0; i < nameList.length; i++) {
-        
-          charList.push(nameList[i].data)
-          
-        this.setState({characters: charList})
-        
-       // console.log(this.state.characters)
-        }
-        
-      }))
+    let findClickedMovie = this.state.movies
       
-     }//if
-    }//forLoop
+      for (var i=0; i < findClickedMovie.length; i++) {
+        if(findClickedMovie[i].title === movie) {
+        this.setState({movieTitle: findClickedMovie[i].title })
     
-  }//showMovie
+        axios.all(findClickedMovie[i].characters.map(chars => axios.get(chars)))
+        .then(axios.spread((...result) => {
+          let ListOfCharactersInMovie = result
+          let ListOfCharactersToShow = []
+       
+            for(let i=0; i < ListOfCharactersInMovie.length; i++) {
+              ListOfCharactersToShow.push(ListOfCharactersInMovie[i].data)
+          
+  this.setState({characters: ListOfCharactersToShow })
+        }//forLoop characters
+      }))//axios call characters
+    }//ifStatmentFindMovie
+  }//forLoopFindMovie
+}//showMovie
 
-  hideMovie = () => {
-    this.setState({showInfoMovie: false})
-  }
+hideMovie = () => {
+  this.setState({characters: []})
+  this.setState({showInfoMovie: false})
+}//hideMovie
   
 render() {
   return (
-    <div className="Films">
+    <div className="films">
       <h3>All Star Wars Movies:</h3>
-        <ul>
-          { this.state.movies.length === 0
-            ? <h2>Loading movies...</h2>
-            :
-            this.state.movies.map(movie => <li key={movie.title}>
-          <button onClick={() => this.showMovie(movie.title)}>Star Wars, Episode:{movie.episode_id} Date: {movie.release_date}</button>
-          
-        </li>)}
-      </ul>
-      {
-        this.state.showInfoMovie ?
-        <div className="movieInfo">
-           <button onClick={() => this.hideMovie()}>Go Back</button>
-
-            {this.state.movieTitle}
+        <h4>In asscending order by release date:</h4>
           <ul>
-         {this.state.characters.map(chars => <li key={chars.name}>
-         <p>{chars.name}</p>
-         </li>)}
-        </ul>
-          </div>
+            {this.state.movies.length === 0
+              ? <div className="loadingScreen"><h2>Loading movies...</h2></div>
+              :
+                this.state.movies.map(movie => 
+            <li key={movie.title}>
+              <button className="showMovie" onClick={() => this.showMovie(movie.title)}>
+                {movie.title} Episode:{movie.episode_id} Release Date: {movie.release_date}</button>
+            </li>)}
+          </ul>
+            
+        {this.state.showInfoMovie ?
+          <div className="movieInfo">
+            <button className="goBack" onClick={() => this.hideMovie()}>Go Back</button>
+              <p className="nameOfEpisode">Name of Epiode: {this.state.movieTitle}</p>
+              <p className="chars">Characters in this episode:</p>
+            <ul className="ulCharacters">
+                {this.state.characters.map(chars => 
+              <li className="listCharacters" key={chars.name}>
+                <p>Name: {chars.name}</p>
+              </li>)}
+            </ul>
+        </div>//movieInfo
         :null
       }
-      
-    </div>
-    );
-    }
-  }
-  export default Films;
+    </div>//Div Films
+    );//return
+  }//render
+}//Films Component
+  
+export default Films;
   
