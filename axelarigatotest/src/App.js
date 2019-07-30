6dc877789cfd4223ef6ebe2876bbb625d22d6422
erik.johansson <erikjohansson890@gmail.com>
2019-07-30 16:04:43 +0200

    done

--- axelarigatotest/src/App.js
+++ axelarigatotest/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Films from './components/Films'
+import Films from './components/Films/Films'
 
 class App extends Component { 
   constructor(props) {
